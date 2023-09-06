e9ff7691a644fab2e97bb5bc2fe5b60b27fec265
Sebastian Gilits <sep.gil@gmail.com>
2023-09-06 12:45:54 +0000

    Implement inpainting prediction

--- predict.py
+++ predict.py
@@ -2,21 +2,107 @@
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
 from cog import BasePredictor, Input, Path
+from diffusers import (
+    StableDiffusionXLInpaintPipeline,
+    DDIMScheduler,
+    DPMSolverMultistepScheduler,
+    EulerAncestralDiscreteScheduler,
+    EulerDiscreteScheduler,
+    HeunDiscreteScheduler,
+    PNDMScheduler,
+)
+from diffusers.utils import load_image
+import torch
+import os
+import shutil
+
+SDXL_MODEL_CACHE = "sdxl-cache"
+
+
+class KarrasDPM:
+    def from_config(config):
+        return DPMSolverMultistepScheduler.from_config(config, use_karras_sigmas=True)
+
+
+SCHEDULERS = {
+    "DDIM": DDIMScheduler,
+    "DPMSolverMultistep": DPMSolverMultistepScheduler,
+    "HeunDiscrete": HeunDiscreteScheduler,
+    "KarrasDPM": KarrasDPM,
+    "K_EULER_ANCESTRAL": EulerAncestralDiscreteScheduler,
+    "K_EULER": EulerDiscreteScheduler,
+    "PNDM": PNDMScheduler,
+}
+
+
+def get_image(path):
+    shutil.copyfile(path, "/tmp/image.png")
+    return load_image("/tmp/image.png").convert("RGB")
 
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
-        # self.model = torch.load("./weights.pth")
+        self.__pipe = StableDiffusionXLInpaintPipeline.from_pretrained(
+            SDXL_MODEL_CACHE,
+            torch_dtype=torch.float16,
+            variant="fp16",
+            use_safetensors=True,
+        ).to("cuda")
 
+    @torch.inference_mode()
     def predict(
         self,
-        image: Path = Input(description="Grayscale input image"),
-        scale: float = Input(
-            description="Factor to scale image by", ge=0, le=10, default=1.5
+        prompt: str = Input(
+            description="Input prompt",
+            default="An astronaut riding a rainbow unicorn",
+        ),
+        negative_prompt: str = Input(
+            description="Input Negative Prompt",
+            default="",
+        ),
+        image: Path = Input(
+            description="Input image for img2img or inpaint mode",
+            default=None,
+        ),
+        mask: Path = Input(
+            description="Input mask for inpaint mode. Black areas will be preserved, white areas will be inpainted.",
+            default=None,
+        ),
+        num_inference_steps: int = Input(
+            description="Number of denoising steps",
+            ge=1,
+            le=500,
+            default=50
+        ),
+        guidance_scale: float = Input(
+            description="Scale for classifier-free guidance",
+            ge=1,
+            le=50,
+            default=7.5
+        ),
+        prompt_strength: float = Input(
+            description="Prompt strength when using img2img / inpaint. 1.0 corresponds to full destruction of information in image",
+            ge=0.0,
+            le=1.0,
+            default=0.8,
+        ),
+        seed: int = Input(
+            description="Random seed. Leave blank to randomize the seed",
+            default=None
         ),
     ) -> Path:
         """Run a single prediction on the model"""
-        # processed_input = preprocess(image)
-        # output = self.model(processed_image, scale)
-        # return postprocess(output)
+
+        images = self.__pipe(
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+            image=get_image(image),
+            mask_image=get_image(mask),
+        ).images
+
+        output_path = f"/tmp/out.png"
+
+        images[0].save(output_path)
+
+        return Path(output_path)
